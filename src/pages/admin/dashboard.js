c21bb2fa0095b555bcbf4699db6bcb93ff8ddc3d
Mydtph <79831056+MyPH13712@users.noreply.github.com>
2022-01-15 16:20:46 +0700

    update

--- src/pages/admin/dashboard.js
+++ src/pages/admin/dashboard.js
@@ -9,7 +9,7 @@ const DashBoardPage = {
             <header class="bg-white shadow">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                 <h1 class="text-3xl font-bold text-gray-900">
-                Dashboard
+                Dashboard hello
                 </h1>
             </div>
             </header>
