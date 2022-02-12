1b507a1e75f4e77df8e13a3593cc08fa7526d64c
Mydtph <79831056+MyPH13712@users.noreply.github.com>
2022-02-12 17:54:34 +0700

    logout

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,5 @@
+import { reRender } from "../utils";
+
 const Header = {
     render() {
         return /* html */`
@@ -14,9 +16,12 @@ const Header = {
                 <li><a href="/product" class="block px-3 py-3 hover:bg-blue-800 hover:text-white hover:underline">Sản phẩm</a></li>
                 <li><a href="/admin/dashboard" class="block px-3 py-3 hover:bg-blue-800 hover:text-white hover:underline">Thống kê</a></li>
             </ul>
+            ${localStorage.getItem("user") ? `
             <ul class="flex">
-                <li class="flex items-center">Xin chao <a href="/" class="block px-4 py-3 hover:bg-blue-800 hover:text-white" id="email"></a></li>
-            </ul>
+              <li class="flex items-center">Xin chao <a href="/" class="block px-4 py-3 hover:bg-blue-800 hover:text-white" id="email"></a></li>
+              <li class="flex items-center"><a class="block px-4 py-3 hover:bg-blue-800 hover:text-white" id="logout">Logout</a></li>
+          </ul>
+          ` : ""}
         </div>
         </nav>`;
     },
@@ -24,10 +29,18 @@ const Header = {
         // Lấy thông tin từ localStorage
         // Sử dụng JSON.parse để chuyển đổi chuỗi sang object
         const email = document.querySelector("#email");
+        const logout = document.querySelector("#logout");
         if (email) {
             email.innerHTML = JSON.parse(localStorage.getItem("user")).email;
         }
+        if (logout) {
+            logout.addEventListener("click", () => {
+                localStorage.removeItem("user");
+                reRender(Header, "#header");
+            });
+        }
     },
+
 };
 
 export default Header;
