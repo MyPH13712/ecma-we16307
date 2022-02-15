1819b1c38c2623e984f47b32e1ceb9af07393a5c
Mydtph <79831056+MyPH13712@users.noreply.github.com>
2022-02-15 10:00:22 +0700

    Update news.js

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,7 +1,10 @@
-import data from "../data";
+// eslint-disable-next-line no-unused-vars
+import axios from "axios";
+import { getAll } from "../api/posts";
 
 const NewsPage = {
-    render() {
+    async render() {
+        const response = await getAll();
         return /* html */ `
         <div class="max-w-5xl mx-auto">
             <header>
@@ -9,9 +12,9 @@ const NewsPage = {
             <section class="news">
                 <h2 class="max-w-5xl mx-auto font-semibold text-2xl text-sky-800 uppercase my-4">Tin tức</h2>
                 <div class="grid grid-cols-3 gap-8 max-w-5xl mx-auto">
-                ${data.map((post) => `
+                ${response.data.map((post, index) => `
                         <div class="border p-3">
-                            <a href="/news/${post.id}">
+                            <a href="/news/${index + 1}">
                                 <img src="${post.img}"  />
                             </a>
                             <h3 class="my-3"><a href="/news/${post.id}" class="font-semibold text-lg text-orange-500 ">${post.title}</a></h3>
