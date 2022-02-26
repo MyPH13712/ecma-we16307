e4f4017543d1e86fe20eabc8b1e9fd90efcd18ae
Mydtph <79831056+MyPH13712@users.noreply.github.com>
2022-02-26 21:22:17 +0700

    Update index.js

--- src/pages/admin/posts/index.js
+++ src/pages/admin/posts/index.js
@@ -64,7 +64,7 @@ const AdminPosts = {
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                                   <a href="/admin/posts/${post.id}/edit" class="text-indigo-600 hover:text-indigo-900">Sửa</a>
-                                  <button data-id="${post.id}" class="inline-block bg-indigo-500 hover:bg-indigo-800 text-white text-sm py-2 px-6 mx-4 rounded">Xóa</button>
+                                  <button data-id="${post.id}" class="btn inline-block bg-indigo-500 hover:bg-indigo-800 text-white text-sm py-2 px-6 mx-4 rounded">Xóa</button>
                                 </td>
                               </tr>
                               `).join("")}
